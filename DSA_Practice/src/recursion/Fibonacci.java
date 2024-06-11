b5e40980d96888a3794cb7fed822f3b4a6a1441e
saumyasingh <saumya.singh@nagarro.com>
2024-06-11 22:37:37 +0530

    Print First N Natural Number program done

--- DSA_Practice/src/recursion/Fibonacci.java
+++ DSA_Practice/src/recursion/Fibonacci.java
@@ -7,10 +7,17 @@ public class Fibonacci {
 	public static void main(String[] args) {
 		Scanner sc= new Scanner(System.in);
 		System.out.println("Enter how many numbers do you want to print in Fibonacci series:");
-
+		int limit=sc.nextInt();
+		fib(limit);
 	}
-	static void fib(int limit) {
-		
+	static void fib(int n) {
+		if(n==1||n==2) {
+			System.out.println("1");
+			return;
+		}
+		else {
+			
+		}
 	}
 
 }
