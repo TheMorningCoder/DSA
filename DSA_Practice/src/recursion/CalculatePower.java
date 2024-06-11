534266bcdcf48f4a6df9d04749c27b2a0529e4b3
saumyasingh <saumya.singh@nagarro.com>
2024-06-11 14:55:22 +0530

    Calculate Power Program Completed

--- DSA_Practice/src/recursion/CalculatePower.java
+++ DSA_Practice/src/recursion/CalculatePower.java
@@ -1,10 +1,26 @@
 package recursion;
-
+import java.util.Scanner;
 public class CalculatePower {
 
 	public static void main(String[] args) {
-		System.out.println("Hey Saumya");
-
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Calculate power of a number");
+		System.out.println("Enter number");
+		int x=sc.nextInt();
+		System.out.println("Enter power");
+		int n=sc.nextInt();
+		int power=calc_pow(x,n);
+		System.out.println(power);
+	}
+	static int calc_pow(int x,int n){
+		int pow;
+		if(n==0) {
+			return 1;
+		}
+		else {
+			return x * calc_pow(x,n-1);
+		}
+		
 	}
 
 }
