f018ea832c9683093af50546b2d58abc7329c09c
saumyasingh <saumya.singh@nagarro.com>
2024-06-11 23:32:35 +0530

    Fibonacci Series Program Completed

--- DSA_Practice/src/recursion/Fibonacci.java
+++ DSA_Practice/src/recursion/Fibonacci.java
@@ -1,23 +1,28 @@
 package recursion;
-import java.util.Scanner;
 
+import java.util.Scanner;
 
 public class Fibonacci {
 
 	public static void main(String[] args) {
-		Scanner sc= new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter how many numbers do you want to print in Fibonacci series:");
-		int limit=sc.nextInt();
-		fib(limit);
-	}
-	static void fib(int n) {
-		if(n==1||n==2) {
-			System.out.println("1");
-			return;
-		}
-		else {
-			
+		int limit = sc.nextInt();
+		// Print the first N numbers
+		for (int i = 0; i < limit; i++) {
+
+			System.out.print(fib(i) + " ");
 		}
 	}
+
+	static int fib(int n)
+    {
+        // Base Case
+        if (n <= 1)
+            return n;
+
+        // Recursive call
+        return fib(n - 1) + fib(n - 2);
+    }
 
 }
