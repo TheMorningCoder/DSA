0ecec3cd7ef14b89b553b682483a7f026d7ecac2
saumyasingh <saumya.singh@nagarro.com>
2024-06-14 12:20:52 +0530

    Program to check if Array is Sorted through Recursion Completed

---
+++ DSA_Practice/src/recursion/Check_If_Array_Is_Sorted.java
@@ -0,0 +1,29 @@
+package recursion;
+import java.util.Scanner;
+
+public class Check_If_Array_Is_Sorted {
+	public static void main(String [] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the length of array");
+		int len=sc.nextInt();
+		int arr[]=new int[len];
+		System.out.println("Enter array");
+		for(int i=0;i<len;i++) {
+			arr[i]=sc.nextInt();
+		}
+		System.out.println(checkArray(arr));
+	}
+	public static boolean checkArray(int a[]) {
+		if(a.length==1) {
+			return true;
+		}
+		if(a[0]>a[1]) {
+			return false;
+		}
+		int newArray[]=new int[a.length-1];
+		for(int i=1;i<a.length;i++) {
+			newArray[i-1]=a[i];
+		}
+		return checkArray(newArray);
+	}
+}
