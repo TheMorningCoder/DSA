103b5cf5b318fa1852bf8543d5ddf210dc9b4e06
saumyasingh <saumya.singh@nagarro.com>
2024-06-21 11:29:55 +0530

    Remove Consecutive Duplicates from ArrayList

---
+++ DSA_Practice/src/linked_list/RemoveConsecutiveDupesFromArrayList.java
@@ -0,0 +1,39 @@
+package linked_list;
+
+import java.util.ArrayList;
+
+class RemoveConsecutiveDupesFromArrayList {
+
+    public static ArrayList<Integer> removeDupes(ArrayList<Integer> arr) {
+    	
+    	ArrayList<Integer> newArr=new ArrayList<>();
+    	newArr.add(arr.get(0));
+    	for(int i=1;i<arr.size();i++) {
+    		if(arr.get(i)!=arr.get(i-1)) {
+    			newArr.add(arr.get(i));
+    		}
+    	}
+    	
+    	return newArr;
+    }
+	
+	
+	
+    public static void main(String args[]) {
+    	ArrayList<Integer> arr=new ArrayList<>();
+    	ArrayList<Integer> newArr=new ArrayList<>();
+    	arr.add(1);
+    	arr.add(1);
+    	arr.add(1);
+    	arr.add(2);
+    	arr.add(2);
+    	arr.add(3);
+    	for(int a:arr)
+    		System.out.print(a+" ");
+    	System.out.println();
+    	newArr=removeDupes(arr);
+    	for(int a:newArr)
+    		System.out.print(a+" ");
+    	System.out.println();
+    }
+}
